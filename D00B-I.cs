e40d24440c91a4aead9cde54277b199f4328a7a8
Andrew S. Bantly <andybantly@hotmail.com>
2024-04-04 18:33:54 -0500

    ASB - 4/4/2024 - Move ListViewItemComparer into a new file

--- D00B-I.cs
+++ D00B-I.cs
@@ -1572,38 +1572,4 @@ namespace D00B
         }
         #endregion // THREADING
     }
-    // Not currently used anymore due to virtual listviews.  For non-virtual this can be used
-    class ListViewItemComparer : IComparer
-    {
-        private readonly int m_iColumn;
-        private readonly bool m_bAscending;
-        public ListViewItemComparer()
-        {
-            m_iColumn = 0;
-            m_bAscending = false;
-        }
-
-        public ListViewItemComparer(int iColumn, bool bAscending) : this()
-        {
-            m_iColumn = iColumn;
-            m_bAscending = bAscending;
-        }
-        public ListViewItemComparer(int iColumn)
-        {
-            m_iColumn = iColumn;
-        }
-        public int Compare(object lhs, object rhs)
-        {
-            string sLhs = ((ListViewItem)lhs).SubItems[m_iColumn].Text;
-            string sRhs = ((ListViewItem)rhs).SubItems[m_iColumn].Text;
-            bool bLhsNum = int.TryParse(sLhs, out int iLhs);
-            bool bRhsNum = int.TryParse(sRhs, out int iRhs);
-
-            bool bNumber = (bLhsNum && bRhsNum) ? true : false;
-            if (bNumber)
-                return m_bAscending ? (iLhs < iRhs ? -1 : (iLhs == iRhs ? 0 : 1)) : (iLhs < iRhs ? 1 : (iLhs == iRhs ? 0 : -1));
-            else
-                return m_bAscending ? string.Compare(sLhs, sRhs) : string.Compare(sRhs, sLhs); // Do we need insensitive?
-        }
-    }
 }
---
+++ ListViewItemComparer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D00B
+{
+    class ListViewItemComparer : IComparer
+    {
+        private readonly int m_iColumn;
+        private readonly bool m_bAscending;
+        public ListViewItemComparer()
+        {
+            m_iColumn = 0;
+            m_bAscending = false;
+        }
+
+        public ListViewItemComparer(int iColumn, bool bAscending) : this()
+        {
+            m_iColumn = iColumn;
+            m_bAscending = bAscending;
+        }
+        public ListViewItemComparer(int iColumn)
+        {
+            m_iColumn = iColumn;
+        }
+        public int Compare(object lhs, object rhs)
+        {
+            string sLhs = ((ListViewItem)lhs).SubItems[m_iColumn].Text;
+            string sRhs = ((ListViewItem)rhs).SubItems[m_iColumn].Text;
+            bool bLhsNum = int.TryParse(sLhs, out int iLhs);
+            bool bRhsNum = int.TryParse(sRhs, out int iRhs);
+
+            bool bNumber = (bLhsNum && bRhsNum) ? true : false;
+            if (bNumber)
+                return m_bAscending ? (iLhs < iRhs ? -1 : (iLhs == iRhs ? 0 : 1)) : (iLhs < iRhs ? 1 : (iLhs == iRhs ? 0 : -1));
+            else
+                return m_bAscending ? string.Compare(sLhs, sRhs) : string.Compare(sRhs, sLhs); // Do we need insensitive?
+        }
+    }
+}
